22a0d55eabf70865152c8fba2ac5beff09d6e975
derickfan <fanderick9999@gmail.com>
2020-01-15 00:16:56 -0500

    added css

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/scripts.js', (req, res) => {
     res.sendFile(path.join(__dirname + '/scripts.js'))
 })
 
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/style.css'))
+})
+
 const server = http.listen(port, function() {
     console.log(`listening on *: ${port}`);
 });
